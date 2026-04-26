2a311dd9b365d9a4cc31b45891c06ab4f0519ca1
cayman-code <levlitvinenko2008@gmail.com>
2026-04-26 03:59:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,13 @@
-const CACHE = 'isoworld-v1';
+const CACHE = 'colony7-v1';
 const FILES = ['./', './index.html', './manifest.json'];
-
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {})));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{})));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).catch(()=>caches.match('./index.html'))));
 });
